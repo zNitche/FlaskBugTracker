0045272567beb99e28ce15689a6a47cc14e9701b
zNitche <53448802+zNitche@users.noreply.github.com>
2022-10-16 21:10:16 +0200

    improved forms html rendering

--- flask_bug_tracker/app_modules/forms.py
+++ flask_bug_tracker/app_modules/forms.py
@@ -1,9 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired
 
 
-class LoginForm(FlaskForm):
+class FormBase(FlaskForm):
+    def __init__(self):
+        super().__init__()
+
+        self.html_ignored_fields = ["CSRFTokenField"]
+
+
+class LoginForm(FormBase):
     email = StringField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
-    submit = SubmitField("Login")
