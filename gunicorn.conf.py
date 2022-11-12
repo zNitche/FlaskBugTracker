86228c20192398f7e36f8fdb04e716124aed5bf6
zNitche <53448802+zNitche@users.noreply.github.com>
2022-11-12 14:28:30 +0100

    tweaked gunicorn config

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -5,10 +5,10 @@ from config import Config
 bind = f"{Config.APP_HOST}:{Config.APP_PORT}"
 
 
-workers = multiprocessing.cpu_count() - 1
+workers = multiprocessing.cpu_count() * 2 - 1
 threads = 2
 worker_class = "gthread"
 worker_connections = 1000
 
-timeout = 20
+timeout = 10
 keepalive = 5
